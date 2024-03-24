062e0dc2595711a19e2fac243b0163eb381233d6
Senanyo Benwari <benwarisenanyo@yahoo.com>
2024-03-24 20:56:09 +0000

    testCurrentDb

--- backend/counterbase/settings.py
+++ backend/counterbase/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,6 +96,8 @@ DATABASES = {
     }
 }
 
+database_url = os.getenv('POSTGRES_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -138,10 +145,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://frontend-umber-zeta-58.vercel.app/"
+    "https://frontend-umber-zeta-58.vercel.app"
     ]
 CORS_ORIGIN_WHITELIST = [
-    "https://frontend-umber-zeta-58.vercel.app/"
+    "https://frontend-umber-zeta-58.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
